d837c1c2c0c9b55d78836d909dcb26c6ad852712
cokumujr <cokumujnr@gmail.com>
2026-03-04 20:32:31 +0300

    feat(ui): add blog components,signup page and navbar

---
+++ mhealth-blog/components/navbar.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { ThemeToggle } from "./themeToggle";
+
+export function Navbar() {
+    return (
+        <nav className="w-full flex items-center justify-between py-6">
+            <div className="flex items-center space-x-2">
+                <Link href="/">
+                <h1 className="text-3xl font-bold">
+                <span className="text-blue-500">Nadra</span>
+                </h1>
+                </Link>
+            </div>
+
+                <div className="px-10 flex items-center gap-4 ">
+                    <Link href="/" className={ buttonVariants({ variant: "ghost", size:"lg" }) }>
+                        Home
+                    </Link>
+                    <Link href="/blog" className={ buttonVariants({ variant: "ghost" ,size:"lg"})}>
+                        Blog
+                    </Link>
+                    <Link href="/create" className={ buttonVariants({ variant: "ghost",size:"lg" })}>
+                        Create
+                    </Link>
+
+                </div>
+            
+
+            <div className="flex items-center gap-2">
+                <Link href="/auth/signup" className={ buttonVariants({ variant: "outline" ,size:"lg"})}>
+                    Sign Up
+                </Link>
+                <Link href="/auth/login" className={ buttonVariants({ variant: "default",size:"lg" })}>
+                    
+                    Login
+                </Link>
+                <ThemeToggle />
+            </div>
+            
+        </nav>
+    )
+}
